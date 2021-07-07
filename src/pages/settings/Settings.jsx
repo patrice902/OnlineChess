c2e277d9ba2a7a41845c36b98aa01b7b3f2d33bf
patrice902 <patricereddick902@yahoo.com>
2021-07-07 10:49:17 -0400

    update settings page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -21,7 +21,7 @@ export const Settings = () => {
         Settings
       </Typography>
 
-      <Grid container spacing={2}>
+      <Grid container spacing={2} mb={5}>
         <Grid item xs={10} sm={10}>
           <Typography variant="subtitle1">
             Allow anonymous users to view your stream
@@ -29,6 +29,44 @@ export const Settings = () => {
           <Typography variant="body1" color="textSecondary">
             By default we do not stream your video to users who are not a
             certified member of the platform
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Switch color="secondary" />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} mb={5}>
+        <Grid item xs={10} sm={10}>
+          <Typography variant="subtitle1">Input moves with keyboard</Typography>
+          <Typography variant="body1" color="textSecondary">
+            Input moves with keyboard
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Switch color="secondary" />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} mb={5}>
+        <Grid item xs={10} sm={10}>
+          <Typography variant="subtitle1">Kid mode</Typography>
+          <Typography variant="body1" color="textSecondary">
+            We will disable chat and video sharing for kids, if you feel they
+            should not be visible please enable this setting
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Switch color="secondary" />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} mb={5}>
+        <Grid item xs={10} sm={10}>
+          <Typography variant="subtitle1">Two factor authentication</Typography>
+          <Typography variant="body1" color="textSecondary">
+            Activate two factor authentication, and make sure your account is
+            safe and secure.
           </Typography>
         </Grid>
         <Grid item xs={2}>
