25ea2bec3bcb6fff6023f16fb00c8bec3192e1df
richard929 <richard.zhan929@gmail.com>
2021-07-09 11:57:41 -0400

    update prize balance to 0

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -140,7 +140,7 @@ export const Dashboard = () => {
               </Box>
               <Box mb={3}>
                 <Typography variant="h1" component="p">
-                  $4,500.00
+                  $0.00
                 </Typography>
               </Box>
               <TextLabel>Available prizes</TextLabel>
