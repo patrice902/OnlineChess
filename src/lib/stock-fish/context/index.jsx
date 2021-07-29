bd1f169d3b25282a00628ea8ba5c74fd68f5d795
richard929 <richard.zhan929@gmail.com>
2021-07-29 15:22:43 -0400

    remove stock fish logs

--- src/lib/stock-fish/context/index.jsx
+++ src/lib/stock-fish/context/index.jsx
@@ -11,7 +11,7 @@ export const StockFishProvider = (props) => {
 
   useEffect(() => {
     // Create a new StockFish Client
-    const stockFishClient = new StockFishClient(0);
+    const stockFishClient = new StockFishClient();
 
     // Initialize
     stockFishClient.initialize();
